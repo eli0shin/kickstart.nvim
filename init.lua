3bc3a46341059fc6f0eb1edfdfc2d1482571b841
Eli Oshinsky <eoshinsky@paypal.com>
2024-05-08 23:12:12 -0400

    initial setup

--- init.lua
+++ init.lua
@@ -22,59 +22,59 @@
 
 What is Kickstart?
 
-  Kickstart.nvim is *not* a distribution.
+Kickstart.nvim is *not* a distribution.
 
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
+Kickstart.nvim is a starting point for your own configuration.
+  The goal is that you can read every line of code, top-to-bottom, understand
+  what your configuration is doing, and modify it to suit your needs.
 
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
+  Once you've done that, you can start exploring, configuring and tinkering to
+  make Neovim your own! That might mean leaving Kickstart just the way it is for a while
+  or immediately breaking it into modular pieces. It's up to you!
 
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
+  If you don't know anything about Lua, I recommend taking some time to read through
+  a guide. One possible example which will only take 10-15 minutes:
+    - https://learnxinyminutes.com/docs/lua/
 
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+  After understanding a bit more about Lua, you can use `:help lua-guide` as a
+  reference for how Neovim integrates Lua.
+  - :help lua-guide
+  - (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
 Kickstart Guide:
 
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
 
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
+  If you don't know what this means, type the following:
+    - <escape key>
+    - :
+    - Tutor
+    - <enter key>
 
-    (If you already know the Neovim basics, you can skip this step.)
+  (If you already know the Neovim basics, you can skip this step.)
 
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
+Once you've completed that, you can continue working through **AND READING** the rest
+of the kickstart init.lua.
 
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
+Next, run AND READ `:help`.
+  This will open up a help window with some basic information
+  about reading, navigating and searching the builtin help documentation.
 
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
+  This should be the first place you go to look when you're stuck or confused
+  with something. It's one of my favorite Neovim features.
 
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
+  MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+  which is very useful when you're not exactly sure of what you're looking for.
 
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
+I have left several `:help X` comments throughout the init.lua
+  These are hints about where to find more information about the relevant settings,
+  plugins or Neovim features used in Kickstart.
 
-   NOTE: Look for lines like this
+ NOTE: Look for lines like this
 
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
+  Throughout the file. These are for you, the reader, to help you understand what is happening.
+  Feel free to delete them once you know what you're doing, but they should serve as a guide
+  for when you are first encountering a few different constructs in your Neovim config.
 
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
 
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -189,6 +189,9 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- remap ; to  ; in normal mode
+vim.keymap.set('n', ';', ':', { remap = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -280,13 +283,64 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
+      local which_key = require 'which-key'
+      which_key.register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       }
+      which_key.register({
+        g = {
+          name = '+Git',
+          h = {
+            name = '+Github',
+            c = {
+              name = '+Commits',
+              c = { '<cmd>GHCloseCommit<cr>', 'Close' },
+              e = { '<cmd>GHExpandCommit<cr>', 'Expand' },
+              o = { '<cmd>GHOpenToCommit<cr>', 'Open To' },
+              p = { '<cmd>GHPopOutCommit<cr>', 'Pop Out' },
+              z = { '<cmd>GHCollapseCommit<cr>', 'Collapse' },
+            },
+            i = {
+              name = '+Issues',
+              p = { '<cmd>GHPreviewIssue<cr>', 'Preview' },
+            },
+            l = {
+              name = '+Litee',
+              t = { '<cmd>LTPanel<cr>', 'Toggle Panel' },
+            },
+            r = {
+              name = '+Review',
+              b = { '<cmd>GHStartReview<cr>', 'Begin' },
+              c = { '<cmd>GHCloseReview<cr>', 'Close' },
+              d = { '<cmd>GHDeleteReview<cr>', 'Delete' },
+              e = { '<cmd>GHExpandReview<cr>', 'Expand' },
+              s = { '<cmd>GHSubmitReview<cr>', 'Submit' },
+              z = { '<cmd>GHCollapseReview<cr>', 'Collapse' },
+            },
+            p = {
+              name = '+Pull Request',
+              c = { '<cmd>GHClosePR<cr>', 'Close' },
+              d = { '<cmd>GHPRDetails<cr>', 'Details' },
+              e = { '<cmd>GHExpandPR<cr>', 'Expand' },
+              o = { '<cmd>GHOpenPR<cr>', 'Open' },
+              p = { '<cmd>GHPopOutPR<cr>', 'PopOut' },
+              r = { '<cmd>GHRefreshPR<cr>', 'Refresh' },
+              t = { '<cmd>GHOpenToPR<cr>', 'Open To' },
+              z = { '<cmd>GHCollapsePR<cr>', 'Collapse' },
+            },
+            t = {
+              name = '+Threads',
+              c = { '<cmd>GHCreateThread<cr>', 'Create' },
+              n = { '<cmd>GHNextThread<cr>', 'Next' },
+              t = { '<cmd>GHToggleThread<cr>', 'Toggle' },
+            },
+          },
+        },
+      }, { prefix = '<leader>' })
     end,
   },
 
@@ -630,7 +684,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettier' } },
+        typescript = { { 'prettier' } },
       },
     },
   },
@@ -739,7 +794,6 @@ require('lazy').setup({
       }
     end,
   },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -754,9 +808,27 @@ require('lazy').setup({
       vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+    enabled = false,
+    init = function()
+      vim.cmd.colorscheme 'carbonfox'
+    end,
+    opts = {
+      options = {
+        styles = {
+          functions = 'italic',
+          keywords = 'italic',
+          operators = 'italic',
+        },
+      },
+    },
+  },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, enabled = false },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -828,6 +900,20 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'ldelossa/gh.nvim',
+    dependencies = {
+      {
+        'ldelossa/litee.nvim',
+        config = function()
+          require('litee.lib').setup()
+        end,
+      },
+    },
+    config = function()
+      require('litee.gh').setup()
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -839,8 +925,8 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -869,6 +955,18 @@ require('lazy').setup({
     },
   },
 })
+
+-- require('tokyonight').setup {
+--   -- Style to be applied to different syntax groups
+--   -- Value is any valid attr-list value for `:help nvim_set_hl`
+--   comments = { italic = true },
+--   keywords = { italic = true },
+--   functions = {},
+--   variables = {},
+--   -- Background styles. Can be "dark", "transparent" or "normal"
+--   sidebars = 'dark', -- style for sidebars, see below
+--   floats = 'dark', -- style for floating windows
+-- }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
