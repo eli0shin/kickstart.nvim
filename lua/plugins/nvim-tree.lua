0d7f31f3415186f0da4b9d89f3db9a01746aa299
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2024-05-18 17:26:20 -0400

    add keybinding to fix session current directory

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,12 +1,14 @@
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
-  enabled = false,
-  lazy = true,
+  enabled = true,
+  lazy = false,
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('nvim-tree').setup {}
+    local tree = require 'nvim-tree'
+    tree.setup {}
+    vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle Nvim [T]ree' })
   end,
 }
