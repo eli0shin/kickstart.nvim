6af424d02b7482fe257ad3d49ae3ae53e737b158
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2024-05-21 11:35:07 -0400

    fix tsserver for tsx

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return { -- Autoformat
   lazy = false,
   keys = {
     {
-      '<leader>f',
+      '<leader>ff',
       function()
         require('conform').format { async = true, lsp_fallback = true }
       end,
@@ -12,14 +12,14 @@ return { -- Autoformat
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
       return {
-        timeout_ms = 500,
+        timeout_ms = 1000,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
@@ -31,7 +31,9 @@ return { -- Autoformat
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
       javascript = { { 'prettier' } },
+      javascriptreact = { { 'prettier' } },
       typescript = { { 'prettier' } },
+      typescriptreact = { { 'prettier' } },
       json = { { 'prettier' } },
     },
   },
