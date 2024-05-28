59befec9cb2ea765897102e25b2de73fbf844ff0
Eli Oshinsky <eoshinsky@paypal.com>
2024-05-28 10:30:08 -0400

    disable annoying lsp progress messages

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,11 +2,22 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add any options here
+    notify = {
+      enabled = true,
+      view = 'mini',
+    },
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+      message = {
+        enabled = false,
+      },
+    },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    'MunifTanjim/nui.nvim',
+    -- 'MunifTanjim/nui.nvim',
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
