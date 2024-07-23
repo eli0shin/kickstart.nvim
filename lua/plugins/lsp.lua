170be5f7f89500bc7b5ca1a2244487adc3dadf12
Eli Oshinsky <eoshinsky@paypal.com>
2024-07-22 20:52:46 -0400

    fix typescript support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -145,7 +145,9 @@ return { -- LSP Configuration & Plugins
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
+      tsserver = {
+        single_file_support = true,
+      },
       --
       -- gleam = {},
 
