0eee9c67ac0e25801234de741b10222f2b09bcea
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2024-11-28 23:29:42 -0500

    Add grug-far

--- init.lua
+++ init.lua
@@ -45,6 +45,7 @@ require('lazy').setup({
   require 'plugins.rest',
   require 'plugins.render-markdown',
   require 'plugins.neogit',
+  require 'plugins.grug-far',
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
