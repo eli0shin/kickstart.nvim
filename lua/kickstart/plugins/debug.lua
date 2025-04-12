7c22f7f1437f3b4aac010ff3fedd99516735cf9d
Eli Oshinsky <eoshinsky@paypal.com>
2025-04-12 12:49:16 -0400

    disable delve and jdtls

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -134,7 +134,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
         'js',
       },
     }
@@ -181,11 +181,11 @@ return {
 
     -- Install golang specific config
     require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
+      -- delve = {
+      --   -- On Windows delve must be run attached or it crashes.
+      --   -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+      --   detached = vim.fn.has 'win32' == 0,
+      -- },
     }
 
     for _, language in ipairs(js_based_languages) do
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -175,7 +175,6 @@ return { -- LSP Configuration & Plugins
       },
       htmx = {},
       html = {},
-      jdtls = {},
       --
       -- gleam = {},
 
