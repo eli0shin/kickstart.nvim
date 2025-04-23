56ba3575185e53918e40894f743ba0a44f968b99
Eli Oshinsky <eoshinsky@paypal.com>
2025-04-23 00:05:17 -0400

    Un-pin Octo version

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,7 +1,7 @@
 return {
   'pwntester/octo.nvim',
-  commit = 'f09ff9413652e3c06a6817ba6284591c00121fe0',
-  pin = true,
+  -- commit = 'f09ff9413652e3c06a6817ba6284591c00121fe0',
+  -- pin = true,
   event = 'VeryLazy',
   requires = {
     'nvim-lua/plenary.nvim',
