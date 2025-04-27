cd5f41d2d849be5f032311121040182a2e8719ac
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2025-04-26 23:40:48 -0400

    Revert "add render-markdown blink.cmp completions"
    This reverts commit ea2412941b4ff2abd4e6d79ef562fcfab1d720c8.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -94,6 +94,5 @@ return { -- Autocompletion
 
     -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
-    completions = { blink = { enabled = true } },
   },
 }
