b80c6f6edd9d56f6bbf754f3f963e7c366d5e321
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2025-05-10 14:14:07 -0400

    don't show result in chat

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -35,7 +35,7 @@ return {
           mcphub = {
             callback = 'mcphub.extensions.codecompanion',
             opts = {
-              show_result_in_chat = true, -- Show mcp tool results in chat
+              show_result_in_chat = false, -- Show mcp tool results in chat
               make_vars = true, -- Convert resources to #variables
               make_slash_commands = true, -- Add prompts as /slash commands
             },
